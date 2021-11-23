3faae6124f6e5e4820851a804fc472cd3ce80203
Allen Coding <allencoding@Allens-MacBook-Pro.local>
2021-11-22 23:02:36 -0500

    Building out netflix clone

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <img className="nav__logo"/>
+            <img className="nav__avatar" src="https://pbs.twig.com/profile_images/124011999041155"/>
+        </div>
+    )
+}
+
+export default Navbar
